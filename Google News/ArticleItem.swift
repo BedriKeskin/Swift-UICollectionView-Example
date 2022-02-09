e92461830c821436d2566cfea73d62a715794830
BedriKeskin <53687943+BedriKeskin@users.noreply.github.com>
2022-02-10 01:58:35 +0300

    ok

---
+++ Google News/ArticleItem.swift
@@ -0,0 +1,39 @@
+//
+//  ArticleItem.swift
+//  Google News
+//
+//  Created by Bedri Keskin on 10.02.2022.
+//  Copyright © 2022 Bedri Keskin. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - NewsApi
+struct NewsApi: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source?
+    let author: String?
+    let title, articleDescription: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String?
+}
--- Google News/ViewController.swift
+++ Google News/ViewController.swift
@@ -13,35 +13,6 @@ import SQLite
 class ViewController: UIViewController {
     var db: Connection?
     let NewsApiTable = Table("NewsApiTable")
-    // MARK: - NewsApi
-    struct NewsApi: Codable {
-        let status: String
-        let totalResults: Int
-        let articles: [Article]
-    }
-    
-    // MARK: - Article
-    struct Article: Codable {
-        let source: Source?
-        let author: String?
-        let title, articleDescription: String?
-        let url: String?
-        let urlToImage: String?
-        let publishedAt: String?
-        let content: String?
-        
-        enum CodingKeys: String, CodingKey {
-            case source, author, title
-            case articleDescription = "description"
-            case url, urlToImage, publishedAt, content
-        }
-    }
-    
-    // MARK: - Source
-    struct Source: Codable {
-        let id: String?
-        let name: String?
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
