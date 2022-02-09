331726c7ae1486b2b987acaf2980ede65fb95424
BedriKeskin <53687943+BedriKeskin@users.noreply.github.com>
2022-02-09 11:01:56 +0300

    Initial Commit

---
+++ Google News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Google News
+//
+//  Created by Bedri Keskin on 9.02.2022.
+//  Copyright © 2022 Bedri Keskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Google NewsTests/Google_NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Google_NewsTests.swift
+//  Google NewsTests
+//
+//  Created by Bedri Keskin on 9.02.2022.
+//  Copyright © 2022 Bedri Keskin. All rights reserved.
+//
+
+import XCTest
+@testable import Google_News
+
+class Google_NewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
